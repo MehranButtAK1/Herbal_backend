eb5f454e1ea12986797fa93476ce44ec2e152220
MehranButtAK1 <130867588+MehranButtAK1@users.noreply.github.com>
2025-08-17 00:17:05 +0500

    Delete routes directory

--- routes/productRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const products = require("../models/Products");
-
-router.get("/", (req, res) => {
-  res.json(products);
-});
-
-module.exports = router;
