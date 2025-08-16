d1fdb1dfb93883e83b7432f593d6cd0b1a9c27c9
MehranButtAK1 <130867588+MehranButtAK1@users.noreply.github.com>
2025-08-16 21:09:45 +0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+
+const productRoutes = require("./routes/productRoutes");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use("/api/products", productRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
