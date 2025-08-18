b5bf46188357a552855879f7491c7dbcb6afb034
MehranButtAK1 <130867588+MehranButtAK1@users.noreply.github.com>
2025-08-19 03:48:09 +0500

    Update error.js

--- middleware/error.js
+++ middleware/error.js
@@ -1,7 +1,5 @@
-module.exports = function errorHandler(err, req, res, next) {
-  console.error("❌ Error:", err);
+export function errorHandler(err, _req, res, _next) {
+  console.error("❌", err);
   const code = err.status || 500;
-  res.status(code).json({
-    error: err.message || "Server error",
-  });
-};
+  res.status(code).json({ error: err.message || "Server error" });
+}
