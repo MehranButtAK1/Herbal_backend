d43c019bc9b3fc1051842882a8f296791f201ddd
MehranButtAK1 <130867588+MehranButtAK1@users.noreply.github.com>
2025-08-19 03:50:47 +0500

    Update user.routes.js

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,26 +1,6 @@
-const router = require("express").Router();
-const User = require("../models/User");
-const auth = require("../middleware/auth");
-const requireAdmin = require("../middleware/admin");
+import { Router } from "express";
+const router = Router();
 
-// List users (admin)
-router.get("/", auth, requireAdmin, async (req, res, next) => {
-  try {
-    const users = await User.find().select("-password").sort({ createdAt: -1 });
-    res.json(users);
-  } catch (e) {
-    next(e);
-  }
-});
+router.get("/", (_req, res) => res.json([]));
 
-// Delete user (admin)
-router.delete("/:id", auth, requireAdmin, async (req, res, next) => {
-  try {
-    await User.findByIdAndDelete(req.params.id);
-    res.json({ success: true });
-  } catch (e) {
-    next(e);
-  }
-});
-
-module.exports = router;
+export default router;
